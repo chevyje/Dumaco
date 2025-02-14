2bbe8e266ca8586b13d8e0937bbbb2eb466a90c8
chevyje <jurre.blankers@gmail.com>
2025-02-14 10:28:43 +0100

    documentation for the status codes, and a route for checking the login credentials

--- backend/api/users/users.js
+++ backend/api/users/users.js
@@ -8,5 +8,21 @@ UsersRouter.get('/getUsers', async (req, res) => {
     res.json(users);
   });
 
+UsersRouter.post('/checkCredentials', async (req, res) => {
+    const users = await db_query("SELECT * FROM users WHERE username = ?", [req.body.username]);
+
+    if(users.length <= 0){
+      res.status(401).json({message: "Incorrect username"})
+      return;
+    }
+
+    if(users[0].password === req.body.password){
+      res.status(200).json({message: "Succesfull Login"});
+      return;
+    }else{
+      res.status(401).json({message: "Incorrect password"});
+    }
+});
+
 export default UsersRouter;
 
