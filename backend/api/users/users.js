160997ffedd7ff82a5376d388bfec25ed454a887
chevyje <jurre.blankers@gmail.com>
2025-02-18 20:36:17 +0100

    connection with frontend for user verification

--- backend/api/users/users.js
+++ backend/api/users/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from 'bcryptjs';
 import { db_query } from "../../db.js";
 
 const UsersRouter = express.Router();
@@ -9,19 +10,34 @@ UsersRouter.get('/getUsers', async (req, res) => {
   });
 
 UsersRouter.post('/checkCredentials', async (req, res) => {
-    const users = await db_query("SELECT * FROM users WHERE username = ?", [req.body.username]);
+    const users = await db_query("SELECT password FROM users WHERE username = ?", [req.body.username]);
 
     if(users.length <= 0){
       res.status(401).json({message: "Incorrect username"})
       return;
     }
 
-    if(users[0].password === req.body.password){
-      res.status(200).json({message: "Succesfull Login"});
-      return;
-    }else{
-      res.status(401).json({message: "Incorrect password"});
-    }
+    bcrypt.compare(req.body.password, users[0].password, (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({message: "There went something wrong on the server"});
+        } else if (result) {
+          res.status(200).json({message: "Succesfull Login"});
+        } else {
+          res.status(401).json({message: "Incorrect password"});
+          console.log(req.body.password)
+          console.log(users[0].password)
+        }
+      });
+});
+
+UsersRouter.post('/getSalt', async (req, res) => {
+  const users = await db_query("SELECT salt FROM users WHERE username = ?", [req.body.username]);
+  if(users.length <= 0){
+    res.status(401).json({message: "Incorrect username"})
+    return;
+  }
+  res.status(200).json(users);
 });
 
 export default UsersRouter;
