bf4e83cb5c06b03b7d5d1fd0726b7a938f2273f5
chevyje <jurre.blankers@gmail.com>
2025-03-10 08:51:35 +0100

    login page fix?
    added cors to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import config from "./config.js";
 import APIRouter from "./api/index.js";
 import { initDB } from "./db.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
