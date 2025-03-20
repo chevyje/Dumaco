3e24fc1fe2549281585a64e80827b715a1013427
chevyje <jurre.blankers@gmail.com>
2025-03-20 14:06:15 +0100

    rename naar dumaco

---
+++ frontend/dumaco/src/pages/klantenOverzicht.jsx
@@ -0,0 +1,12 @@
+import Main from "../layouts/Main.jsx";
+
+
+function klantOverzicht() {
+    return(
+        <>
+            <h1>Test</h1>
+        </>
+    )
+}
+
+export default klantOverzicht;
