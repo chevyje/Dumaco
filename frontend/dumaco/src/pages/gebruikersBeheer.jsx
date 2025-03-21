2ebe9fac08c219bd8c73e99417f575436a36fe72
Aangepast <tobiaslbeumer@gmail.com>
2025-03-21 15:48:59 +0100

    Table title is een ding

---
+++ frontend/dumaco/src/pages/gebruikersBeheer.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import Table from "../components/table.jsx";
+
+function GebruikersBeheer() {
+    const [state, setState] = useState({
+        columns: [],
+        columnsToHide: ["id"],
+        results: [
+            { id: 0, name: "Tobias", lastLogin: "18" },
+            { id: 1, name: "Henk", lastLogin: "38" },
+            { id: 2, name: "Tasdasdasdasd", lastLogin: "300"}
+        ]
+    });
+
+    const columnAlignments = {
+        name: "left",
+        lastLogin: "left"
+    };
+
+    useEffect(() => {
+        if (state.results.length > 0) {
+            setState((prevState) => ({
+                ...prevState,
+                columns: Object.keys(state.results[0])
+            }));
+        }
+    }, [state.results]);
+
+    return (
+        <>
+            <div className="table-container">
+                <Table
+                    columns={state.columns}
+                    columnsToHide={state.columnsToHide}
+                    data={state.results}
+                    primaryColor="#0D20E7"
+                    secondaryColor="#E1ECFF"
+                    tertiareColor="#A8C3FF"
+                    columnAlignments={columnAlignments}
+                    title="test"
+                />
+                <Table
+                    columns={state.columns}
+                    columnsToHide={state.columnsToHide}
+                    data={state.results}
+                    primaryColor="#0D20E7"
+                    secondaryColor="#E1ECFF"
+                    tertiareColor="#A8C3FF"
+                    columnAlignments={columnAlignments}
+                />
+                <Table
+                    columns={state.columns}
+                    columnsToHide={state.columnsToHide}
+                    data={state.results}
+                    primaryColor="#0D20E7"
+                    secondaryColor="#E1ECFF"
+                    tertiareColor="#A8C3FF"
+                    columnAlignments={columnAlignments}
+                />
+                <Table
+                    columns={state.columns}
+                    columnsToHide={state.columnsToHide}
+                    data={state.results}
+                    primaryColor="#0D20E7"
+                    secondaryColor="#E1ECFF"
+                    tertiareColor="#A8C3FF"
+                    columnAlignments={columnAlignments}
+                />
+            </div>
+        </>
+    );
+}
+
+export default GebruikersBeheer;
