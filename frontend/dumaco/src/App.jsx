99f28411b58c798b89473e3a8136c3ff9da5fc8b
Marty <144609720+martyparty06@users.noreply.github.com>
2025-03-24 13:42:15 +0100

    Instellingen

--- frontend/dumaco/src/App.jsx
+++ frontend/dumaco/src/App.jsx
@@ -6,7 +6,7 @@ import GebruikersBeheer from "./pages/gebruikersBeheer.jsx";
 import GebruikersBeheerEdit from "./pages/gebruikersBeheerEdit.jsx";
 import TeamsBeheer from "./pages/TeamsBeheer.jsx";
 import OrderbonnenFabriek from "./pages/orderbonnenFabriek.jsx";
-
+import Instellingen from "./pages/instellingen.jsx";
 
 
 
@@ -21,6 +21,7 @@ const App = () => {
                     <Route path="/gebruikersbeheeredit" element={<GebruikersBeheerEdit/>} />
                     <Route path="/TeamsBeheer" element={<TeamsBeheer/>} />
                     <Route path="/orderbonnenfabriek" element={<OrderbonnenFabriek />} />
+                    <Route path="/instellingen" element={<Instellingen />} />
                 </Routes>
             </Main>
         </Router>
---
+++ frontend/dumaco/src/pages/instellingen.jsx
@@ -0,0 +1,13 @@
+import Main from "../layouts/Main.jsx";
+import React from "react";
+
+function instellingen() {
+    return(
+        <>
+            <h1>Instellingen</h1>
+            <p>testt</p>
+        </>
+    )
+}
+
+export default instellingen;
