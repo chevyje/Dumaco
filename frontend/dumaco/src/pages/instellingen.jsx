d84d21240a2cf8718230bcd8092fc010e15569a5
Marty <144609720+martyparty06@users.noreply.github.com>
2025-03-24 14:36:00 +0100

    Instellingen 0.1

--- frontend/dumaco/src/pages/instellingen.jsx
+++ frontend/dumaco/src/pages/instellingen.jsx
@@ -1,11 +1,16 @@
 import Main from "../layouts/Main.jsx";
 import React from "react";
+import "../styling/instellingen.css";
+
+
+
+
 
 function instellingen() {
     return(
         <>
-            <h1>Instellingen</h1>
-            <p>testt</p>
+            <h1 className="instellingen-title">Instellingen</h1>
+            <p className="instellingen-text">testt</p>
         </>
     )
 }
