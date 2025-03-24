36ebe24362f64f9fb4f3e84902a161c3ab9b94ad
chevyje <jurre.blankers@gmail.com>
2025-03-24 10:30:46 +0100

    merge conflict resolve

--- frontend/dumaco/src/App.jsx
+++ frontend/dumaco/src/App.jsx
@@ -2,7 +2,8 @@ import Main from "./layouts/Main.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import KlantOverzicht from "./pages/klantOverzicht.jsx";
 import KlantenOverzicht from "./pages/klantenOverzicht.jsx";
-import GebruikersEdit from "./pages/gebruikersEdit.jsx";
+import GebruikersBeheer from "./pages/gebruikersBeheer.jsx";
+import GebruikersBeheerEdit from "./pages/gebruikersBeheerEdit.jsx";
 
 
 const App = () => {
@@ -12,7 +13,8 @@ const App = () => {
                 <Routes>
                     <Route path="/klantoverzicht" element={<KlantOverzicht />} />
                     <Route path="/klantenoverzicht" element={<KlantenOverzicht />} />
-                    <Route path="/gebruikersedit" element={<GebruikersEdit />} />
+                    <Route path="/gebruikersbeheer" element={<GebruikersBeheer />} />
+                    <Route path="/gebruikersbeheeredit" element={<GebruikersBeheerEdit/>} />
                 </Routes>
             </Main>
         </Router>
