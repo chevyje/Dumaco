1d16e91856b5a1093bd551900bf294f4baa957dd
Aangepast <tobiaslbeumer@gmail.com>
2025-03-25 16:03:12 +0100

    onnodige data weggehaald

--- frontend/dumaco/src/pages/gebruikersBeheer.jsx
+++ frontend/dumaco/src/pages/gebruikersBeheer.jsx
@@ -9,18 +9,6 @@ function GebruikersBeheer() {
             { id: 0, name: "Tobias", lastLogin: "18" },
             { id: 1, name: "Henk", lastLogin: "38" },
             { id: 2, name: "Tasdasdasdasd", lastLogin: "300"},
-            { id: 3, name: "Tobias", lastLogin: "18" },
-            { id: 4, name: "Henk", lastLogin: "38" },
-            { id: 5, name: "Tasdasdasdasd", lastLogin: "300"},
-            { id: 6, name: "Tobias", lastLogin: "18" },
-            { id: 7, name: "Henk", lastLogin: "38" },
-            { id: 8, name: "Tasdasdasdasd", lastLogin: "300"},
-            { id: 9, name: "Tobias", lastLogin: "18" },
-            { id: 10, name: "Henk", lastLogin: "38" },
-            { id: 11, name: "Tasdasdasdasd", lastLogin: "300"},
-            { id: 12, name: "Tobias", lastLogin: "18" },
-            { id: 13, name: "Henk", lastLogin: "38" },
-            { id: 14, name: "Tasdasdasdasd", lastLogin: "300"},
         ]
     });
 
