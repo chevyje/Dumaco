5d82c5b487e5711dbd244c91185c6f0bca81aa7f
Marty <144609720+martyparty06@users.noreply.github.com>
2025-03-26 11:54:26 +0100

    Instellingen v0.2

--- frontend/dumaco/src/pages/instellingen.jsx
+++ frontend/dumaco/src/pages/instellingen.jsx
@@ -1,18 +1,82 @@
 import Main from "../layouts/Main.jsx";
 import React from "react";
 import "../styling/instellingen.css";
+import Button from "../components/button.jsx";
+import {useNavigate} from "react-router-dom";
+import InputField from "../components/inputField.jsx";
 
-
-
-
-
-function instellingen() {
-    return(
+const Instellingen = () => {
+    const navigate = useNavigate();
+    return (
         <>
-            <h1 className="instellingen-title">Instellingen</h1>
-            <p className="instellingen-text">testt</p>
+            <Main />
+            <div className={"username-container"}>
+                <p className={"username-text"}>Hans Botterboy</p>
+            </div>
+            <div className="inputField-container">
+                <div className="form-container">
+                    <div className="column">
+                        <InputField
+                            title={"Naam"}
+                            placeholder={"..."}
+                            defaultText={"Hans Botterboy"}
+                            fieldId={"naam"}
+                            isLocked={false}
+                        />
+                        <InputField
+                            title={"Herstel email"}
+                            placeholder={"voorbeeld@domein.com"}
+                            defaultText={""}
+                            fieldId={"recoverymail"}
+                            isLocked={false}
+                        />
+                        {<a className={"reset-password"}>Reset wachtwoord</a>}
+                        <InputField
+                            title={"Functie"}
+                            placeholder={"functie titel"}
+                            defaultText={"Lasser"}
+                            fieldId={"function"}
+                            isLocked={false}
+                        />
+                    </div>
+                    <div className="column">
+
+                    </div>
+                </div>
+            </div>
+            <div className={"btns-container"}>
+                <Button
+                    title={"Opslaan"}
+                    triggerFunction={saveUser}
+                    color={'#2eb822'}
+                    icon={"save"}
+                />
+                <Button
+                    title={"Annuleren"}
+                    triggerFunction={() => cancelEdit(navigate)}
+                    color={'#757575'}
+                    icon={"ban"}
+                />
+            </div>
+            <div className={"delete-btn"}>
+                <Button
+                    title={""}
+                    triggerFunction={null}
+                    color={'#710000'}
+                    icon={"trash"}
+                />
+            </div>
         </>
     )
+};
+
+
+function saveUser(){
+    console.log("saveUser() function called.");
 }
 
-export default instellingen;
+function cancelEdit(navigate){
+    navigate("/Instellingen");
+}
+
+export default Instellingen;
