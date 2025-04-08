4018df81283926172b2a3f877de435b8e0d70566
Tomevandoorne <Tomevandoorne@gmail.com>
2025-04-08 11:01:05 +0200

    laatste commit voor deze branch

---
+++ frontend/dumaco/src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import Style from "./Card.module.css"
+   
+
+    function Card() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+export default Card
