66aec937e3e432678953f1945e843db71777a344
Tomevandoorne <Tomevandoorne@gmail.com>
2025-04-09 15:12:46 +0200

    halve home pagina
    ik geef het op ik ben klaar met dat kut icon

--- frontend/dumaco/src/components/Card/card.jsx
+++ frontend/dumaco/src/components/Card/card.jsx
@@ -1,12 +1,15 @@
 import Style from "./card.module.css"
    
 
-    function Card({H1Stats, pStats, PSoort, children }) {
+    function Card({H1Stats, icon , pStats, PSoort, children }) {
+        
+        const Icon = "../../../icons/" + icon + ".svg";
+
         return(
             <div className={Style.cardContainer}>
                 <div className={`${Style.cardContentContainer} ${Style.cardTopRow}`}>
                     <h1 className={Style.cardH1}>{H1Stats}</h1>
-                    <p className={Style.cardPStats}>{pStats}</p>
+                    <p className={Style.cardPStats}>{icon && <img className={Style.btnIcon} src={Icon} alt={"button"}></img>}{pStats}</p>
                 </div>
                 <div className={Style.cardContentContainer}>
                     <p className={Style.cardPSoort}>{PSoort}</p>
