4a7577765d1ef82aae655dcd5007e86369a38ec1
Aangepast <tobiaslbeumer@gmail.com>
2025-05-11 17:13:34 +0200

    Fabriek taak kleine progressie moest werken

--- frontend/dumaco/src/pages/taakInzicht/TaakInzicht.jsx
+++ frontend/dumaco/src/pages/taakInzicht/TaakInzicht.jsx
@@ -1,5 +1,6 @@
 import breadRouteGen from "../../components/navbar/breadRouteGen.js";
 import Navbar from "../../components/navbar/navbar.jsx";
+import Style from "./TaakInzicht.module.css";
 
 function taakInzicht() {
 
@@ -14,6 +15,20 @@ function taakInzicht() {
     return (
         <>
             <Navbar title={"Taak"} route={route}/>
+            <div className={Style.mainContainer}>
+                <div className={Style.contentContainer}>
+                    <div className={Style.contentDescription}></div>
+                    <div className={Style.contentLocation}></div>
+                    <div className={Style.viewDrawing}>
+                        <div className={Style.viewDrawingIcon}>
+                            <img src={`../../../icons/image-upscale.svg`} alt={"imageUpscale"} className={Style.icon}></img>
+                        </div>
+                        <div className={Style.viewDrawingText}>
+                            <p>Bekijk tekening</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
