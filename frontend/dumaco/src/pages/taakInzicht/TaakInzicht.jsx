2d3496383a0f2f7ac24baac74dc1b7f5b7d018e0
Aangepast <tobiaslbeumer@gmail.com>
2025-05-11 17:22:46 +0200

    contentContainer van Taak inzicht af

--- frontend/dumaco/src/pages/taakInzicht/TaakInzicht.jsx
+++ frontend/dumaco/src/pages/taakInzicht/TaakInzicht.jsx
@@ -17,8 +17,14 @@ function taakInzicht() {
             <Navbar title={"Taak"} route={route}/>
             <div className={Style.mainContainer}>
                 <div className={Style.contentContainer}>
-                    <div className={Style.contentDescription}></div>
-                    <div className={Style.contentLocation}></div>
+                    <div className={Style.contentDescription}>
+                        <h3 className={Style.contentDescriptionTitle}>Opmerkingen</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac dictum nisl. Pellentesque eget porta ligula. Quisque id blandit nunc, non sagittis purus. Aliquam erat volutpat. Nunc sed magna et nunc accumsan malesuada sit amet vel leo.</p>
+                    </div>
+                    <div className={Style.contentLocation}>
+                        <h3 className={Style.contentDescriptionTitle}>Locatie</h3>
+                        <p className={Style.locationDescription}>Zetten Dynacel</p>
+                    </div>
                     <div className={Style.viewDrawing}>
                         <div className={Style.viewDrawingIcon}>
                             <img src={`../../../icons/image-upscale.svg`} alt={"imageUpscale"} className={Style.icon}></img>
