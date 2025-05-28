bfec1809cc29d003085937f71655db3cfb419ed4
tobia <tobiaslbeumer@gmail.com>
2025-05-28 10:56:36 +0200

    Basis

---
+++ frontend/dumaco/src/pages/PalletsMap/palletsMap.jsx
@@ -0,0 +1,42 @@
+import Navbar from "../../components/navbar/navbar.jsx";
+import breadRouteGen from "../../components/navbar/breadRouteGen.js";
+import { Stage, Layer, Rect, Circle, Text } from 'react-konva';
+import Style from './palletsMap.module.css'
+
+function PalletsMap () {
+
+    const route = breadRouteGen({
+        "/home": "Home",
+        "/pallets": "Pallets",
+    });
+
+    return (
+        <>
+            <Navbar title={"Pallets"} route={route} />
+            <div className={Style.stage}>
+                <Stage width={window.innerWidth} height={(window.innerHeight)}>
+                    <Layer>
+                        <Rect
+                            x={20}
+                            y={50}
+                            width={100}
+                            height={100}
+                            fill="red"
+                            shadowBlur={10}
+                            draggable
+                        />
+                        <Circle
+                            x={200}
+                            y={100}
+                            radius={50}
+                            fill="green"
+                            draggable
+                        />
+                    </Layer>
+                </Stage>
+            </div>
+        </>
+    );
+}
+
+export default PalletsMap;
